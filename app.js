83f989eee8a81ddcb474d25ba1688f0e925d2727
srinathgandhari <srinathgandhari@gmail.com>
2025-01-09 19:08:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("start");
+// global-scope
+var a = 10
+console.log(a);
+//local-scope
+let b =20;
+console.log(b);
+const c = 30
+console.log(c);
+console.log("end");
+//block-scope
+{
+    var m = 100;
+    console.log(m);
+    let n = 200; 
+    console.log(n);
+    const o = 300;
+    console.log(o);
+    
+
+}
